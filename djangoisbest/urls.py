e097c4c8868c51308e31df5dbe88e22b94ccfc80
TigerCow <doorBW@outlook.com>
2018-02-26 21:05:47 +0900

    6

--- djangoisbest/urls.py
+++ djangoisbest/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls', namespace='blog')),
+    url(r'', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
